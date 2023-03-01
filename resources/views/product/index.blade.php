9916eb2a0217577fd9e2324d64e24e259fa111b1
Paradokas <black.artem.t@yandex.ru>
2023-03-01 14:50:39 +0300

    update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,13 +2,18 @@
 @section('title', 'product')
 @section('content')
     <div class="container mt-3">
-        @foreach ($collections as $collection)
-            <div class="row">
+        <div class="row">
+            @php($i = 1)
+            @foreach ($collections as $collection)
+                @if($i % 3 === 0)
+                    </div>
+                    <div class="row">
+                    @php($i = 1)
+                @endif
                 <div class="col-sm-4">
                     <div class="card mb-3">
                         <div class="card-body">
                             <h1>{{ $collection->title }}</h1>
-
                         </div>
                     </div>
                 </div>
@@ -17,12 +22,14 @@
                         <div class="card mb-3">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $product->title }}</h5>
-                                <p class="card-text">Цена: {{ $product->price }} Коллекция: {{ $collection->title }}</p>
+                                <p class="card-text">Цена: {{ $product->price }} | {{ $collection->title }}</p>
                             </div>
                         </div>
                     </div>
+                    @php($i++)
                 @endforeach
-            </div>
-        @endforeach
+                @php($i++)
+            @endforeach
+        </div>
     </div>
 @endsection
